b7406a89b826c10293e01066e45061497b4ecf05
elupi8 <82871449+elupi8@users.noreply.github.com>
2021-08-31 13:10:34 +0200

    Update merge_ventricle_area_with_platemap.R

--- merge_ventricle_area_with_platemap.R
+++ merge_ventricle_area_with_platemap.R
@@ -10,17 +10,10 @@ rm(list=ls())
   #library(openxlsx)
 }
 
-# read.xlsx <- xlsx::read.xlsx
-# write.xlsx <- xlsx::write.xlsx
-
-
 
 ## function
 readCSV = function(fileIn) {
   
-  ###
-  #fileIn = s.files[1]
-  ###
   print(fileIn)
   
   
@@ -32,29 +25,20 @@ readCSV = function(fileIn) {
   # delete first column
   dt_loc = dt_loc[, V1 := NULL] # or dt_loc[, V1 := NULL]
   
-  # extract name of the well form the filename
+  # extract name of the well from the filename
   
   s.well.loc = str_extract(fileIn, "[:alpha:][:digit:]{3}")
   s.well.loc = toupper(s.well.loc)
   s.well = paste0( str_extract(s.well.loc, "[:alpha:]") , sprintf( '%03d', as.numeric( str_extract(s.well.loc, '[0-9]+') ) ) )
   
   
-  dt_loc = dt_loc[, "Well" := s.well] #define a new column.. if the column already exists and we want to update its value by reference to the same column, use (Gene):= fun(Gene)
-  
+  dt_loc = dt_loc[, "Well" := s.well] #define a new column.. 
   
   return(dt_loc)
 }
 
 
 s.exp = "esp25"
-
-
-# cellLine1 = "M0921" #M0921
-# cellLine2 = "MM12" #MM12
-# s.library = "kinase"
-# s.condition = "starvation"
-# s.meas = "normCNratio" # "zscore" or "normCNratio
-# 
 
 
 # define directory with merged output
@@ -72,14 +56,8 @@ ifelse( !dir.exists(output.Directory), dir.create(output.Directory, recursive = 
 
 
 # define the filename that you want to read or a string contained in the filenames
-s.files.core = 'deconvolved_heart.csv'
+s.files.core = 'deconvolved_heart.csv' #input file name
 
-
-
-
-# Import and prepare an object.table that contains all the plates from both cell lines--------
-
-# search subdirectories for csv files with Nuclei data
 
 s.files = list.files(
   path = file.path(input.Directory,"tables"),
@@ -88,27 +66,19 @@ s.files = list.files(
   full.names = TRUE
 )
 
-
-
-
 s.files  # check that it contains the correct paths to the csv files
-
 
 ## Load csv files by applying the  custom made function readCSV to all the elements whose paths are specified in s.files
 dt_segmentation = rbindlist(lapply(s.files, readCSV), use.names=TRUE, fill=TRUE, idcol=NULL) #rbindlist (l, ...) is not faster (less than 3% faster) than do.call(rbind, l) as stated in R documentation; # dt.img = do.call(rbind, lapply(s.files.img, myFreadImg))
 #dt_epicardium = dt_segmentation[,.(cell.epicardium= max(V1)), by =Well]
 
-output.filename = paste0("heart", s.exp,".csv")
+output.filename = paste0("heart", s.exp,".csv") #otput file name
 fwrite(dt_segmentation, file = file.path(output.Directory, output.filename), row.names=FALSE)
 
 
-# Data cleaning -----------------------------------------------------------
+#merge file with the platemap containing: name of the drugs, concentration etc..
 
-
-
-## read platemap
-
-input.file.pm = file.path(input.Directory, "platemap", "platemap.csv")
+input.file.pm = file.path(input.Directory, "platemap", "platemap.csv") #import platempa
 
 dt_platemap = fread(input.file.pm, header = T)
 
@@ -116,11 +86,11 @@ dt_platemap = dt_platemap[!(Well %in% c("", NA) ),]
 
 
 ## padd well number 
-dt_platemap$Well = toupper(dt_platemap$Well)
+dt_platemap$Well = toupper(dt_platemap$Well) #capital letter
 dt_platemap$Well = paste0( str_extract(dt_platemap$Well, "[:alpha:]") , sprintf( '%03d', as.numeric( str_extract(dt_platemap$Well, '[0-9]+') ) ) )
 
 
-# merge platemap with main data table
+# merge platemap with main data table according to the column Well
 
 dt_epicardium = merge(dt_segmentation,
                       dt_platemap,
@@ -130,6 +100,6 @@ dt_epicardium = merge(dt_segmentation,
 
 # Save as -----------------------------------------------------------------
 
-output.filename = paste0("Merged","_heart","_of_", s.exp,".csv")
+output.filename = paste0("Merged","_heart","_of_", s.exp,".csv") #output file name
 fwrite(dt_epicardium, file = file.path(output.Directory, output.filename), row.names=FALSE)
 
